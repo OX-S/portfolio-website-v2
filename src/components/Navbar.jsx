d39cc5d921689e0a4c1bcd5a676412c997387aa0
OX-S <finn.kliewer1@gmail.com>
2025-02-27 16:29:40 -0600

    moved SVGs to seperate files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import siteContent from "../content/siteContent";
 import { ThemeContext } from '../context/ThemeContext';
+import { ReactComponent as DarkThemeIcon } from '../assets/icons/dark-theme.svg';
+import { ReactComponent as LightThemeIcon } from '../assets/icons/light-theme.svg';
 
 
 function Navbar() {
@@ -52,35 +54,8 @@ function Navbar() {
                         checked={theme === "dark"}
                         aria-label="Toggle Dark/Light Theme"
                     />
-
-                    <svg
-                        className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round">
-                        <circle cx="12" cy="12" r="5"/>
-                        <path
-                            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>
-                    </svg>
-                    <svg
-                        className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round">
-                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-                    </svg>
+                    <LightThemeIcon className="stroke-base-100 fill-base-100 col-start-1 row-start-1"/>
+                    <DarkThemeIcon className="stroke-base-100 fill-base-100 col-start-2 row-start-1"/>
                 </label>
             </div>
         </div>
