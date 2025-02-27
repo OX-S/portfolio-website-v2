f5a2e6f5685aac0b0af426170c5dcb7f9142508c
OX-S <finn.kliewer1@gmail.com>
2025-02-27 16:27:01 -0600

    improved header more

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React, { useContext } from "react";
+// src/components/Navbar.js
+
+import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import siteContent from "../content/siteContent";
 import { ThemeContext } from '../context/ThemeContext';
 
+
 function Navbar() {
-    const { links } = siteContent.navbar;
+    const { brand, links } = siteContent.navbar;
     const { theme, toggleTheme } = useContext(ThemeContext);
 
     const prefetch = (link) => {
@@ -21,11 +24,10 @@ function Navbar() {
 
     return (
         <div className="navbar bg-base-100 px-4 relative">
-            {/* Top Left: Name */}
             <div className="absolute top-0 left-0 mt-4 ml-4">
-                <span className="text-lg font-semibold">Finn Kliewer</span>
-            </div>
+                <span className="text-lg font-semibold">{brand}</span>
 
+            </div>
             {/* Centered Navigation Items */}
             <div className="flex flex-1 justify-center items-center">
                 <div className="hidden lg:flex space-x-4">
@@ -41,8 +43,6 @@ function Navbar() {
                     ))}
                 </div>
             </div>
-
-            {/* Top Right: Theme Toggle */}
             <div className="absolute top-0 right-0 mt-4 mr-4">
                 <label className="grid cursor-pointer place-items-center">
                     <input
@@ -52,6 +52,7 @@ function Navbar() {
                         checked={theme === "dark"}
                         aria-label="Toggle Dark/Light Theme"
                     />
+
                     <svg
                         className="stroke-base-100 fill-base-100 col-start-1 row-start-1"
                         xmlns="http://www.w3.org/2000/svg"
@@ -62,10 +63,10 @@ function Navbar() {
                         stroke="currentColor"
                         strokeWidth="2"
                         strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <circle cx="12" cy="12" r="5" />
-                        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+                        strokeLinejoin="round">
+                        <circle cx="12" cy="12" r="5"/>
+                        <path
+                            d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4"/>
                     </svg>
                     <svg
                         className="stroke-base-100 fill-base-100 col-start-2 row-start-1"
@@ -77,9 +78,8 @@ function Navbar() {
                         stroke="currentColor"
                         strokeWidth="2"
                         strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+                        strokeLinejoin="round">
+                        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
                     </svg>
                 </label>
             </div>
