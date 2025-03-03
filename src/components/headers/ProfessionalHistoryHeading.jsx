2f687ab5ddc6d31cc6f35bdb4d473529c7ea5283
OX-S <finn.kliewer1@gmail.com>
2025-03-03 17:57:50 -0600

    ProfessionalHistoryHeading.jsx scales better now

--- src/components/headers/ProfessionalHistoryHeading.jsx
+++ src/components/headers/ProfessionalHistoryHeading.jsx
@@ -3,23 +3,23 @@ import '../../index.css';
 
 const ProfessionalHistoryHeading = () => {
     return (
-    <div className="flex items-start justify-start mt-6">
-        <div className="inline-block">
-            <h1 className="text-3xl sm:text-6xl font-extrabold text-left drop-shadow-2xl">
-                <span className="block font-extrabold drop-shadow-2xl">
-                    Building the{' '}
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary animate-shimmer">
-                        Future
+        <div className="flex items-start justify-start mt-6">
+            <div className="inline-block">
+                <h1 className="text-3xl sm:text-4xl xl:text-6xl font-extrabold text-left drop-shadow-2xl">
+                    <span className="block font-extrabold drop-shadow-2xl">
+                        Building the{' '}
+                        <span className=" text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary animate-shimmer">
+                            Future
+                        </span>
+                        ,
                     </span>
-                    ,
-                </span>
-                <span className="block mt-2 text-3xl sm:text-6xl font-light">
-                    One Line of Code at a Time
-                </span>
-            </h1>
-            <hr className="mt-6 border-t-4 border w-full mb-16"/>
+                    <span className="block mt-2  text-3xl sm:text-4xl xl:text-6xl font-light">
+                        One Line of Code at a Time
+                    </span>
+                </h1>
+                <hr className="mt-6 border-t-4 border w-full mb-16"/>
+            </div>
         </div>
-    </div>
     );
 };
 
